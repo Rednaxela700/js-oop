45289d0de5448d50033e17117bdf8905110fea41
rednaxela700 <ohleg@windowslive.com>
2020-01-04 22:06:38 +0100

    prototype.js init

---
+++ prototype.js
@@ -0,0 +1,13 @@
+//Constructor
+function Book(title, author, year) {
+  this.title = title;
+  this.author = author;
+  this.year = year;
+  this.getSummary = () => {
+    return `${this.title} written by ${this.author} in ${this.year}`
+  }
+}
+
+//Instantiate an object
+const book1 = new Book('Book1', 'John Doe', 2014)
+const book2 = new Book('Book2', 'Jane Doe', 2001)
