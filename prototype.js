8098b2e4b91014269fc44b1ddb2ca134feef796b
rednaxela700 <ohleg@windowslive.com>
2020-01-04 22:19:09 +0100

    added getAge prototype to book object

--- prototype.js
+++ prototype.js
@@ -10,6 +10,13 @@ Book.prototype.getSummary = function() {
   return `${this.title} written by ${this.author} in ${this.year}`
 }
 
+//getAge
+Book.prototype.getAge = function() {
+  const years = new Date().getFullYear()
+  const yearsDiff = years - this.year
+  return `${this.title} is ${yearsDiff} years old`
+}
+
 //Instantiate an object
 const book1 = new Book('Book1', 'John Doe', 2014)
 const book2 = new Book('Book2', 'Jane Doe', 2001)
