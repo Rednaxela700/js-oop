9721a5414590bc560c5dc6f6ddac99a45d652df4
rednaxela700 <ohleg@windowslive.com>
2020-01-05 11:29:58 +0100

    deleted unneccessary content

--- subclasses.js
+++ subclasses.js
@@ -7,23 +7,4 @@ class Book {
   getSummary() {
     return `${this.title} written by ${this.author} in ${this.year}`;
   }
-  getAge() {
-    const years = new Date().getFullYear();
-    const yearsDiff = years - this.year;
-    return `${this.title} is ${yearsDiff} years old`;
-  }
-  revise(newYear) {
-      this.year = newYear,
-      this.revised = true;
-  }
-  
-  static bestEmployee() {
-    return `The best employee is... Someone!`
-  }
 }
-
-//Instantiate object
-const book1 = new Book("book1", "Jane Doe", 2013);
-book1.revise(2005)
-console.log(book1.getSummary());
-console.log(Book.bestEmployee())
