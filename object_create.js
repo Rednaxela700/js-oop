15e841b266ffa0bc650e40d5acd63b699ddd2101
rednaxela700 <ohleg@windowslive.com>
2020-01-05 11:14:29 +0100

    created book1 object using Object.create

--- object_create.js
+++ object_create.js
@@ -9,3 +9,13 @@ const bookProtos = {
     return `${this.title} is ${yearsDiff} years old`
   }
 }
+
+//create object
+const book1 = Object.create(bookProtos)
+book1.title = 'book1'
+book1.author = 'John Dafoe'
+book1.year = 2007
+
+
+console.log(book1.getSummary())
+console.log(book1.getAge())
