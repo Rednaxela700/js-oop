cc25d5acc22ab2931a5d238f25d52f9e26901cfd
rednaxela700 <ohleg@windowslive.com>
2020-01-05 11:27:01 +0100

    added methods to Book class

--- classes.js
+++ classes.js
@@ -1,7 +1,24 @@
 class Book {
   constructor(title, author, year) {
-    this.title = title
-    this.author = author
-    this.year = year
+    this.title = title;
+    this.author = author;
+    this.year = year;
+  }
+  getSummary() {
+    return `${this.title} written by ${this.author} in ${this.year}`;
+  }
+  getAge() {
+    const years = new Date().getFullYear();
+    const yearsDiff = years - this.year;
+    return `${this.title} is ${yearsDiff} years old`;
+  }
+  revise(newYear) {
+      this.year = newYear,
+      this.revised = true;
   }
 }
+
+//Instantiate object
+const book1 = new Book("book1", "Jane Doe", 2013);
+book1.revise(2005)
+console.log(book1.getSummary());
