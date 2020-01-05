e2dbbf32e9a68dada606accd8eb54be58e9a985d
rednaxela700 <ohleg@windowslive.com>
2020-01-05 10:09:49 +0100

    added magazine constructor

--- inheritance.js
+++ inheritance.js
@@ -9,3 +9,14 @@ function Book(title, author, year) {
 Book.prototype.getSummary = function() {
   return `${this.title} written by ${this.author} in ${this.year}`
 }
+
+//Magazine constructor
+function Magazine(title, author, year, month) {
+  Book.call(this, title, author, year)
+
+  this.month = month
+}
+
+const mag1 = new Magazine('Mag One', 'John Doe', 2020, 'January')
+
+console.log(mag1)
